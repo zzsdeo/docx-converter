150aa51043703908bb46877b8b1c8415751fb651
zzsdeo <zzsdeo@gmail.com>
2018-10-15 18:09:14 +0300

    ok

--- main.go
+++ main.go
@@ -3,17 +3,41 @@ package main
 import (
 	"baliance.com/gooxml/document"
 	"baliance.com/gooxml/spreadsheet"
+	"io/ioutil"
 	"log"
+	"strings"
+)
+
+const (
+	docxExtension  = ".docx"
+	workingDir     = "."
+	resultXlsxName = "result.xlsx"
 )
 
 func main() {
-	doc, err := document.Open("document.docx")
+	docNames, err := getDocxFileNames(workingDir)
 	if err != nil {
-		log.Fatalf("error opening document: %s", err)
+		log.Fatalf("error opening working dir: %s", err)
 	}
 
 	ss := spreadsheet.New()
 	sheet := ss.AddSheet()
+
+	for _, docName := range docNames {
+		err := convertTable(docName, &sheet)
+		if err != nil {
+			log.Fatalf("error opening document: %s", err)
+		}
+	}
+
+	ss.SaveToFile(resultXlsxName)
+}
+
+func convertTable(docName string, sheet *spreadsheet.Sheet) error {
+	doc, err := document.Open(docName)
+	if err != nil {
+		return err
+	}
 
 	for _, table := range doc.Tables() {
 		for _, row := range table.Rows() {
@@ -21,14 +45,30 @@ func main() {
 			for _, cell := range row.Cells() {
 				var text string
 				for _, p := range cell.Paragraphs() {
-					for _, r := range  p.Runs() {
+					for _, r := range p.Runs() {
 						text += r.Text()
 					}
 				}
 				xlsxRow.AddCell().SetString(text)
 			}
+			xlsxRow.AddCell().SetString(docName)
 		}
 	}
 
-	ss.SaveToFile("document.xlsx")
+	return nil
+}
+
+func getDocxFileNames(dir string) ([]string, error) {
+	fileInfos, err := ioutil.ReadDir(dir)
+	if err != nil {
+		return nil, err
+	}
+
+	var docxFiles []string
+	for _, info := range fileInfos {
+		if !info.IsDir() && strings.HasSuffix(info.Name(), docxExtension) {
+			docxFiles = append(docxFiles, info.Name())
+		}
+	}
+	return docxFiles, nil
 }
