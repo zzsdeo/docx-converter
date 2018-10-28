6648018b69e4d50a86d993ed1cac1fcf69f8fd1e
andrew <zzsdeo@gmail.com>
2018-10-28 21:41:00 +0300

    ok

--- comparator.go
+++ comparator.go
@@ -12,30 +12,26 @@ import (
 )
 
 const (
-	numOfColumns            = 10
-	defaultFirstDocxName    = "./docs/1.docx"
-	defaultSecondDocxName   = "./docs/2.docx"
-	kit                     = "компл."
-	multiLevelListSeparator = "."
-	voidItemName            = "VOID"
+	numOfColumns          = 10
+	defaultFirstDocxName  = "1.docx"
+	defaultSecondDocxName = "2.docx"
+	kit                   = "компл."
+	voidItemName          = "VOID"
 )
 
-var (
-	itemId   int
-	listsMap map[int64][]multiLevelPosition
-)
+var itemId int
 
 type Metadata struct {
 	ID       int
+	fileName string
 	system   string
 	children []*Item
 	parent   *Item
 }
 
 type multiLevelPosition struct {
-	ID       int64
-	position int64
-	level    int64
+	ID    int
+	level int
 }
 
 type Item struct {
@@ -56,25 +52,13 @@ func fire() {
 		reader.ReadString('\n')
 	}()
 
-	doc, err := document.Open(defaultFirstDocxName)
+	slice1, err := convertSpecToSlice(defaultFirstDocxName)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 
-	slice1, err := convertSpecToSlice(doc)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	doc, err = document.Open(defaultSecondDocxName)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	slice2, err := convertSpecToSlice(doc)
+	slice2, err := convertSpecToSlice(defaultSecondDocxName)
 	if err != nil {
 		fmt.Println(err)
 		return
@@ -106,7 +90,7 @@ func fire() {
 			xlsxRow.AddCell().SetString(strconv.FormatFloat(pair[0].quantity, 'f', -1, 64))
 		}
 		xlsxRow.AddCell().SetString(strconv.FormatFloat(pair[1].quantity, 'f', -1, 64))
-		delta := pair[0].quantity - pair[1].quantity
+		delta := pair[1].quantity - pair[0].quantity
 		xlsxRow.AddCell().SetString(strconv.FormatFloat(delta, 'f', -1, 64))
 	}
 
@@ -114,28 +98,14 @@ func fire() {
 
 }
 
-func newListPosition(ID, level int64) multiLevelPosition {
-	if lists, ok := listsMap[ID]; ok {
-		if len(lists) == 0 {
-			mll := multiLevelPosition{ID, 1, level}
-			lists = append(lists, mll)
-			listsMap[ID] = lists
-			return mll
-		}
-
-		lastPosition := lists[len(lists)-1]
-		mll := multiLevelPosition{ID, lastPosition.position + 1, level}
+func convertSpecToSlice(docName string) ([]Item, error) {
+	doc, err := document.Open(docName)
+	if err != nil {
+		return nil, err
 	}
-}
 
-func convertSpecToSlice(doc *document.Document) ([]Item, error) {
 	var slice []Item
-	//for _, def := range doc.Numbering.Definitions() {
-	//	for _, l := range def.Levels() {
-	//		fmt.Println("numbering", l.X()) //todo
-	//	}
-	//}
-	//fmt.Println("numbering", *doc.Numbering.Definitions()[1].Levels()[0].X().LvlText.ValAttr) //todo
+
 	for _, table := range doc.Tables() {
 
 	RowLoop:
@@ -151,30 +121,21 @@ func convertSpecToSlice(doc *document.Document) ([]Item, error) {
 			itemId++
 			item := Item{}
 			item.ID = itemId
+			item.fileName = docName
+			item.position = multiLevelPosition{}
 			for i, cell := range cells {
 				var text string
-				var listLevel int
-				//fmt.Println("cell", cell.Properties()) //todo
 				for _, p := range cell.Paragraphs() {
 					if p.X().PPr != nil && p.X().PPr.NumPr != nil {
-						fmt.Printf("paragraph %s \n", p.X().PPr.NumPr.Ilvl.ValAttr) //todo
-						listLevel = int(p.X().PPr.NumPr.Ilvl.ValAttr)
-					} else {
-						listLevel = 0
+						item.position.ID = int(p.X().PPr.NumPr.NumId.ValAttr)
+						item.position.level = int(p.X().PPr.NumPr.Ilvl.ValAttr)
 					}
 					for _, r := range p.Runs() {
 						text += r.Text()
-						//for _, c := range r.X().EG_RunInnerContent {
-						//	fmt.Printf("inner %s \n", c) //todo
-						//}
 					}
 				}
-				//fmt.Println(text) //todo
 				text = strings.TrimSpace(text)
 				switch i {
-				case 1:
-					item.position = strconv.Itoa(item.ID) + multiLevelListSeparator + strconv.Itoa(listLevel)
-					//fmt.Println("parsed pos", text) //todo
 				case 2:
 					//если наименование не указано, пропускаем
 					if text == "" {
@@ -187,6 +148,11 @@ func convertSpecToSlice(doc *document.Document) ([]Item, error) {
 				case 5:
 					item.vendor = text
 				case 6:
+					//если размерность не указана, пропускаем
+					if text == "" {
+						continue RowLoop
+					}
+
 					item.measure = text
 				case 7:
 					if text != "" {
@@ -292,7 +258,7 @@ func expandKits(slice []Item) []Item {
 	var lastKitItem Item
 	for i, item := range slice {
 		if found {
-			if strings.HasPrefix(item.position, lastKitItem.position) {
+			if item.position.ID == lastKitItem.position.ID && item.position.level > 0 {
 				item.quantity *= lastKitItem.quantity
 				result = append(result, item)
 				continue
@@ -302,11 +268,8 @@ func expandKits(slice []Item) []Item {
 		}
 
 		if item.measure == kit {
-			//fmt.Println("found kit", item.position+" pos") //todo
 			if i+1 < sliceLen {
-				//fmt.Println("found i+1", slice[i+1].position)                              //todo
-				if len(strings.Split(slice[i+1].position, multiLevelListSeparator)) == 2 { //todo check it
-					//fmt.Println(slice[i+1].position, strings.Split(slice[i+1].position, multiLevelListSeparator), len(strings.Split(slice[i+1].position, multiLevelListSeparator))) //todo
+				if slice[i+1].position.level > 0 {
 					found = true
 					lastKitItem = item
 				} else {
